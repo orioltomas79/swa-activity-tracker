7ed65118659c65404779cfa6cb7bd7ec2a960c28
Oriol Tomas <oriol.tomas@benchmarkcapital.co.uk>
2023-07-07 18:23:47 +0200

    amend vite config

--- activity-tracker-ui/vite.config.ts
+++ activity-tracker-ui/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [react()],
-})
+});
