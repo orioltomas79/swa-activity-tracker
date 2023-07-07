e0f2d7633c1e42733819f92c0c7ce787a678d9ce
orioltomas <orioltomas79@hotmail.com>
2023-07-08 00:09:51 +0200

    Home page without chart

--- activity-tracker-ui/src/pages/Home/Home.tsx
+++ activity-tracker-ui/src/pages/Home/Home.tsx
@@ -1,6 +1,31 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Deposits from "../../Components/Deposits";
+import Orders from "../../Components/Orders";
+
 const Home = () => {
   return (
-    <div>Hello</div>
+    <Grid container spacing={3}>
+      {/* Recent Deposits */}
+      <Grid item xs={12} md={4} lg={3}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            height: 240,
+          }}
+        >
+          <Deposits />
+        </Paper>
+      </Grid>
+      {/* Recent Orders */}
+      <Grid item xs={12}>
+        <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+          <Orders />
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
