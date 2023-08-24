6d39c5f94bf1b38c9ecde27d2164b68ccecd6710
Oriol Tomàs <orioltomas79@hotmail.com>
2023-08-24 23:56:58 +0200

    UI: Fix selected option issue

--- activity-tracker-ui/src/features/activities/ActivityAdd.tsx
+++ activity-tracker-ui/src/features/activities/ActivityAdd.tsx
@@ -4,12 +4,9 @@ import { DatePicker } from "@mui/x-date-pickers";
 import { Box, Button, InputLabel, MenuItem, Select } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectActivityTypes } from "../activityTypes/store/selectors";
-import { useState } from "react";
-
 
 export default function ActivityAdd() {
   const activityTypes = useAppSelector(selectActivityTypes).activityTypes;
-  const [selectedOption, setSelectedOption] = useState(activityTypes[0].id);
 
   return (
     <>
@@ -20,7 +17,6 @@ export default function ActivityAdd() {
         <InputLabel>Activity Type</InputLabel>
         <Select
           label="Activty type"
-          value={selectedOption}
         >
           {activityTypes.map((r) => (
             <MenuItem key={r.id} value={r.id}>
